99fd3e4cbeaaf63c710e378d79034c67b476642f
kdivya13 <82749284+kdivya13@users.noreply.github.com>
2023-09-21 19:45:32 +0530

    Add files via upload

---
+++ Appium/Project/Google_Keep.java
@@ -0,0 +1,52 @@
+package Activities;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class Google_Keep {
+    // Driver Declaration
+    AndroidDriver driver;
+    WebDriverWait wait;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.google.android.keep");
+        options.setAppActivity(".activities.BrowseActivity");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+        driver = new AndroidDriver(serverURL, options);
+        //wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    @Test
+    public void GoogleKeep() {
+
+        driver.findElement(AppiumBy.id("com.google.android.keep:id/new_note_button")).click();
+        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[6]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.EditText")).click();
+        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[6]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.EditText")).sendKeys("This is my First Task");
+        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[6]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.EditText")).click();
+        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[6]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.EditText")).sendKeys("This is Task 1");
+        driver.findElement(AppiumBy.accessibilityId("Navigate up")).click();
+        String text= driver.findElement(AppiumBy.id("index_note_title")).getText();
+        Assert.assertEquals(text,"This is my First Task");
+        System.out.println("Task Added");
+
+    }
+
+}
