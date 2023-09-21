6a676447cf775eab2b05169e3cc68736640d13ff
kdivya13 <82749284+kdivya13@users.noreply.github.com>
2023-09-21 18:57:08 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,8 @@
+REM   Script: Activity1
+REM   table creation
+
+create table salesman(salesman_id int,salesman_name varchar2(32),salesman_city varchar2(32),commission int);
+
+describe salesman
+
+
