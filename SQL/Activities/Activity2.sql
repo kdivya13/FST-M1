2c1ed1cb69907fb0134ec7bc74c96f28a2078ed1
kdivya13 <82749284+kdivya13@users.noreply.github.com>
2023-09-21 19:01:48 +0530

    Add files via upload

---
+++ SQL/Activities/Activity2.sql
@@ -0,0 +1,14 @@
+REM   Script: Activity2
+REM   Inserting Data into Salesman
+
+insert all  
+    into salesman values(5002,'Nail Knite','Paris',13)  
+     into salesman values(5005,'Pit Alex','London',11)  
+     into salesman values(5006,'McLyon','Paris',14)  
+     into salesman values(5007,'Paul Adam','Rome',13)  
+     into salesman values(5003,'Lauson Hen','San Jose',12)  
+    select 1 from dual ;
+
+
+select * from salesman;
+
